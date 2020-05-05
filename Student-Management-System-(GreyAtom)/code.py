c85a46aca08f3023e862bf8c8e0c7dcfe083b3d9
Neha48 <Neha48@users.noreply.github.com>
2020-05-05 15:54:41 +0530

    Code file created.

---
+++ Student-Management-System-(GreyAtom)/code.py
@@ -0,0 +1,58 @@
+# --------------
+# Code starts here
+class_1=['Geoffrey Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio']
+# Create the lists 
+class_2=['Hilary Mason','Carla Gentry','Corinna Cortes']
+# Concatenate both the strings
+new_class=class_1+class_2
+print(new_class)
+# Append the list
+new_class.append('Peter Warden')
+# Print updated list
+print(new_class)
+new_class.remove('Carla Gentry')
+print(new_class)
+
+# Remove the element from the list
+
+# Print the list
+courses={'Math':65, 'English':70, 'History':80, 'French':70, 'Science':60}
+# Create the Dictionary
+total=courses.values()
+total=sum(list(total))
+# Slice the dict and stores  the all subjects marks in variable
+
+# Store the all the subject in one variable `Total`
+
+# Print the total
+print(total)
+# Insert percentage formula
+percentage=total*100/500
+print(percentage)
+# Print the percentage
+mathematics={'Geoffrey Hinton':78, 'Andrew Ng':95, 'Sebastian Raschka':65,
+            'Yoshua Benjio':50, 'Hilary Mason':70, 'Corinna Cortes':66}
+topper = max(mathematics,key = mathematics.get)
+print (topper)
+
+# Create the Dictionary
+first_name,last_name=topper.split()
+full_name=last_name+' '+first_name
+certificate_name=full_name.upper()
+
+# Given string
+
+
+# Create variable first_name 
+
+# Create variable Last_name and store last two element in the list
+
+# Concatenate the string
+
+# print the full_name
+
+# print the name in upper case
+
+# Code ends here
+
+
